4ef97f7648263385a57e23a7f9678d824b9a9b54
kudo <minhkudo17797@gmail.com>
2024-11-03 15:18:54 +0700

    commit

--- lib/extension/datetime_extension.dart
+++
@@ -1,5 +0,0 @@
-import 'package:fa_utils/utils/date_time/date_time_ultis.dart';
-
-extension CompareCurrentTime2StringExtension on DateTime {
-  String get compareCurrentTime2String => dateTimeUltis.convertDateTime2String(this);
-}
--- lib/fa_utils.dart
+++ lib/fa_utils.dart
@@ -1,15 +1,4 @@
 library fa_utils;
-//
-// export 'package:fa_utils/extension/common_extension.dart';
-// export 'package:fa_utils/extension/context_extension.dart';
-// export 'package:fa_utils/extension/datetime_extension.dart';
-// export 'package:fa_utils/extension/list_extension.dart';
-// export 'package:fa_utils/extension/map_extension.dart';
-// export 'package:fa_utils/extension/string_extension.dart';
-//
-// export 'package:fa_utils/utils/responsive.dart';
-
-export 'package:sizer/sizer.dart';
 
 /// A Calculator.
 class Calculator {
