6755ce445af942188bc1b5b06b00707dbad5b1f0
kudo <minhkudo17797@gmail.com>
2024-11-04 16:57:47 +0700

    commit

--- lib/fa_utils.dart
+++ lib/fa_utils.dart
@@ -1,6 +1,7 @@
 library fa_utils;
 
 export 'package:photo_manager/photo_manager.dart';
+export 'package:photo_manager_image_provider/photo_manager_image_provider.dart';
 
 /// A Calculator.
 class Calculator {
