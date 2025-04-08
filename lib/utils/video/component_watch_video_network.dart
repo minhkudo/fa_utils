30e4e5941f11c559dfdde601b1c7e026af3dc991
kudo <minhkudo17797@gmail.com>
2025-04-08 19:04:27 +0700

    commit

--- lib/utils/video/component_watch_video_network.dart
+++ lib/utils/video/component_watch_video_network.dart
@@ -1,6 +1,5 @@
-import 'package:chewie/chewie.dart';
+import 'package:better_player_plus/better_player_plus.dart';
 import 'package:flutter/material.dart';
-import 'package:video_player/video_player.dart';
 
 class ComponentWatchVideoNetwork extends StatefulWidget {
   static const ROUTE_NAME = 'ComponentWatchVideoNetwork';
@@ -15,45 +14,30 @@ class ComponentWatchVideoNetwork extends StatefulWidget {
 
 class _ComponentWatchVideoNetworkState extends State<ComponentWatchVideoNetwork> {
   static const TAG = 'ComponentWatchVideoNetwork';
-  late ChewieController _chewieController;
-  late VideoPlayerController _videoPlayerController;
+  late BetterPlayerController _betterPlayerController;
 
   @override
   void initState() {
-    super.initState();
-    _videoPlayerController = VideoPlayerController.networkUrl(Uri.parse(widget.url));
-
-    _videoPlayerController.initialize().then((_) => _videoPlayerController.addListener(() {
-          if (_videoPlayerController.value.position >= _videoPlayerController.value.duration &&
-              !_videoPlayerController.value.isPlaying) {
-            // Auto reset khi kết thúc
-            _videoPlayerController.seekTo(Duration.zero).then((_) => _videoPlayerController.play());
-          }
-        }));
-
-    _chewieController = ChewieController(
-      videoPlayerController: _videoPlayerController,
-      autoInitialize: true,
-      showControls: true,
-      allowFullScreen: true,
-      allowMuting: true,
-      showControlsOnInitialize: false,
-      aspectRatio: _videoPlayerController.value.aspectRatio,
+    _betterPlayerController = BetterPlayerController(
+      BetterPlayerConfiguration(),
+      betterPlayerDataSource: BetterPlayerDataSource(
+        BetterPlayerDataSourceType.network,
+        widget.url,
+      ),
     );
+    super.initState();
   }
 
   @override
   void dispose() {
-    _videoPlayerController.dispose();
-    _chewieController.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return AspectRatio(
-      aspectRatio: _videoPlayerController.value.aspectRatio,
-      child: Chewie(controller: _chewieController),
+      aspectRatio: 16 / 9,
+      child: BetterPlayer(controller: _betterPlayerController),
     );
   }
 }
