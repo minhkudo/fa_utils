ee96343840b1252e37ba9ba443c8dd94bdc36589
kudo <minhkudo17797@gmail.com>
2025-10-15 23:06:40 +0700

    commit

--- lib/extension/either_extension.dart
+++ lib/extension/either_extension.dart
@@ -43,3 +43,69 @@ extension EitherX<L, R> on Either<L, R> {
   /// Dùng trong async/await (convert về Future)
   Future<R> asyncGetOrThrow() async => getOrThrow();
 }
+
+/// Extension cho Future<Either<L, R>> để dùng tiện trong async/await
+extension FutureEitherX<L, R> on Future<Either<L, R>> {
+  /// Lấy Right, nếu Left thì throw Exception
+  Future<R> getOrThrow() async {
+    final either = await this;
+    return either.getOrThrow();
+  }
+
+  /// Lấy Right, nếu Left thì trả null
+  Future<R?> getOrNull() async {
+    final either = await this;
+    return either.getOrNull();
+  }
+
+  /// Lấy Right, nếu Left thì trả fallback
+  Future<R> getOrElse(R fallback) async {
+    final either = await this;
+    return either.getOrElse(fallback);
+  }
+
+  /// Trả về Right nếu có, ngược lại null
+  Future<R?> get rightOrNull async {
+    final either = await this;
+    return either.rightOrNull;
+  }
+
+  /// Trả về Left nếu có, ngược lại null
+  Future<L?> get leftOrNull async {
+    final either = await this;
+    return either.leftOrNull;
+  }
+
+  /// Thực thi nếu là Right
+  Future<void> onRight(void Function(R r) action) async {
+    final either = await this;
+    either.onRight(action);
+  }
+
+  /// Thực thi nếu là Left
+  Future<void> onLeft(void Function(L l) action) async {
+    final either = await this;
+    either.onLeft(action);
+  }
+
+  /// Thực thi cho cả Left/Right (viết gọn cho fold)
+  Future<T> when<T>({
+    required T Function(L l) left,
+    required T Function(R r) right,
+  }) async {
+    final either = await this;
+    return either.when(left: left, right: right);
+  }
+
+  /// Map Right thành kiểu khác
+  Future<Either<L, T>> mapRight<T>(T Function(R r) transform) async {
+    final either = await this;
+    return either.mapRight(transform);
+  }
+
+  /// Map Left thành kiểu khác
+  Future<Either<T, R>> mapLeft<T>(T Function(L l) transform) async {
+    final either = await this;
+    return either.mapLeft(transform);
+  }
+}
