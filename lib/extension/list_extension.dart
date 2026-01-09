e2d36f9b0b84ccb9c2a06f77246c5825c1d97618
kudo <minhkudo17797@gmail.com>
2026-01-09 21:09:33 +0700

    commit

--- lib/extension/list_extension.dart
+++ lib/extension/list_extension.dart
@@ -1,12 +1,10 @@
 extension DifferenceWithSet<T> on List<T> {
-  List<T> differenceWithSet<T>(List<T> a, List<T> b) {
-    return a.toSet().difference(b.toSet()).toList();
+  List<T> differenceWithSet(List<T> other) {
+    return toSet().difference(other.toSet()).toList();
   }
 
-  List<T> update(int index, T t) {
-    List<T> list = [];
-    list.add(t);
-    replaceRange(index, index + 1, list);
+  List<T> updateAt(int index, T value) {
+    this[index] = value;
     return this;
   }
 }
